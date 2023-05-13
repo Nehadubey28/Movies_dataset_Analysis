47613441f019260db45b3f5a5a436e273c7779af
Nehadubey28 <nehadubey20031998@.com>
2023-05-14 00:04:57 +0530

    Initial commit

---
+++ src/Spark_RDD/Distinct_Genres/execute.sh
@@ -0,0 +1,14 @@
+if [ -d "result" ]
+then
+	echo "Removing existing directory"
+	rm -r result
+else
+	echo "Executing File"
+fi
+spark-shell -i list_of_distinct_genres.scala
+echo ""
+echo ""
+echo "The list of distinct genres are"
+echo ""
+cat "result/part-00000"
+echo ""
